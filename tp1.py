6fd7803d2b16a3aaa70d506661fdf7d8dd292dfd
SuchitUppala <79006079+SuchitUppala@users.noreply.github.com>
2021-09-25 16:32:21 +0530

    Update tp1.py

--- tp1.py
+++ tp1.py
@@ -1,7 +1,7 @@
 import hashlib
 from datetime import datetime
- 
- 
+
+# Class for new transaction
 class Transaction:
     def __init__(self, timestamp, amount, name, prev_hash):
         self.timestamp = timestamp
@@ -10,54 +10,57 @@ class Transaction:
         self.name = name
         self.blockdata = str(self.prev_hash) + str(self.timestamp) + self.name + str(self.amount)
         self.hash = hashlib.sha256(bytearray(self.blockdata, "utf-8")).hexdigest()
- 
+
+    # Function to verify the transaction
     def verify(previous_block_hash, block_hash):
         return (previous_block_hash == block_hash)
- 
- 
+
+# Class for new block
 class Block:
-    def __init__(self, transaction, prev_block_hash):
+    def __init__(self, transaction, prev_block_hash, transactionlist):
         self.blockdata = transaction
-        self.prev_block_hash=prev_block_hash
+        self.prev_block_hash = prev_block_hash
         self.blockhash = hashlib.sha256(bytearray(self.blockdata, "utf-8")).hexdigest()
- 
+        self.transactionlist = transactionlist
+
+    # Function to verify the block
     def verify(previous_block_hash, block_hash):
         return (previous_block_hash == block_hash)
- 
- 
+
+
 # Initializing the Blockchain Variables
 i = 0
 cnt = 0
 blockchain = []
 sz = 0
- 
+
+# Instruction messages
 msg = """Type 1 to add a transaction.   
        Type 2 to show the blockchain
        Type 3 to show a block
        Type 4 to show a transaction
        Type 5 to check the integrity of the blockchain
        Type 6 to exit"""
- 
+
 print(msg)
- 
+
+# Initialising lists and strings
 tempchain = []
- 
 transaction = []
- 
 unverified = []
 verified = []
 temphash = ""
 integrity = True
- 
 prevhash = None
- 
+
+# Loop to execute instructions entered by user
 while (True):
     x = int(input())
+    # Adding block to the blockchain and verifying the added block
     if cnt == 3 and sz > 0:
-        z = (int)(sz / 3)-1
-        # t = (int)((sz + 1) / 3)
-        blockchain.append(Block(temphash, blockchain[z].blockhash))
-        t=z+1
+        z = (int)(sz / 3) - 1
+        blockchain.append(Block(temphash, blockchain[z].blockhash, tempchain))
+        t = z + 1
         if (Block.verify(blockchain[t].prev_block_hash, blockchain[z].blockhash)):
             integrity = True
         else:
@@ -67,17 +70,18 @@ while (True):
         tempchain = []
         temphash = ""
         cnt = 0
-        sz+=3
-        
- 
+        sz += 3
+
+    # Adding the genesis block
     elif cnt == 3 and sz == 0:
-        blockchain.append(Block(temphash, None))
+        blockchain.append(Block(temphash, None, tempchain))
         prevhash = tempchain[cnt - 1].hash
         tempchain = []
         temphash = ""
         cnt = 0
         sz += 3
- 
+
+    # Adding the first transaction
     if (x == 1 and i == 0):
         print("Enter The Amount")
         am = int(input())
@@ -93,7 +97,8 @@ while (True):
         temphash += str(tempchain[cnt].hash)
         i += 1
         cnt += 1
- 
+
+    # Adding the other transactions
     elif (x == 1 and i > 0 and cnt > 0):
         print("Enter The Amount ")
         am = int(input())
@@ -119,7 +124,8 @@ while (True):
         temphash += str(tempchain[cnt].hash)
         i += 1
         cnt += 1
- 
+
+    # Adding the other transactions
     elif (x == 1 and i > 0 and cnt == 0):
         print("Enter The Amount ")
         am = int(input())
@@ -136,7 +142,7 @@ while (True):
             transaction.pop()
             cnt -= 1
             i -= 1
- 
+
         print(tempchain[cnt].timestamp)
         print(tempchain[cnt].prev_hash)
         print(tempchain[cnt].hash)
@@ -145,16 +151,19 @@ while (True):
         temphash += str(tempchain[cnt].hash)
         i += 1
         cnt += 1
- 
+
+    # Breaking the loop once 6 is entered by user
     if (x == 6):
         break
- 
+
+    # Verifying the integrity of the blockchain
     if (x == 5):
         if (integrity):
             print("Your blockchain is verified")
         else:
             print("Your blockchain is not verified")
- 
+
+    # Showing the details of block based on index
     if (x == 3):
         print("Type the block which you want to see")
         p = int(input())
@@ -168,17 +177,19 @@ while (True):
             print(transaction[j].amount)
             print(transaction[j].name)
             j += 1
- 
+
+    # Printing the transaction
     if (x == 4):
         print("Type the transaction you want to see ")
         y = int(input())
-        y=y-1
+        y = y - 1
         print(transaction[y].timestamp)
         print(transaction[y].prev_hash)
         print(transaction[y].hash)
         print(transaction[y].amount)
         print(transaction[y].name)
- 
+
+    # Printing the details of all the blocks present in the blockchain
     if (x == 2):
         count = 1
         j = 0
@@ -190,14 +201,14 @@ while (True):
             print(transaction[j].amount)
             print(transaction[j].name)
             j += 1
- 
+
             print(transaction[j].timestamp)
             print(transaction[j].prev_hash)
             print(transaction[j].hash)
             print(transaction[j].amount)
             print(transaction[j].name)
             j += 1
- 
+
             print(transaction[j].timestamp)
             print(transaction[j].prev_hash)
             print(transaction[j].hash)
@@ -205,15 +216,28 @@ while (True):
             print(transaction[j].name)
             j += 1
             count += 1
- 
- 
-if(integrity == False):
+
+# Checking integrity of blockchain
+if (integrity == False):
     print("Blockchain integrity failed")
- 
+
+# Printing the unverified pool
 print("If you want to see the unverified pool, Press 1")
 tt = int(input())
-if (tt):
-    if(len(unverified) == 0):
+
+if (tt==1):
+    if (len(unverified) == 0):
         print("No unverified transactions")
     else:
         print(unverified)
+
+
+# Printing the verified pool
+print("If you want to see the verified pool, Press 2")
+th = int(input())
+
+if (th==2):
+    if (len(verified) == 0):
+        print("No verified transactions")
+    else:
+        print(verified)
